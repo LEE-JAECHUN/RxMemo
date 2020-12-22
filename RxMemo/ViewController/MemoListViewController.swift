de6ac522b22f2a5f86e1396f1b41bcf4badd951b
LEE-JAECHUN <dlwocjs1618@gmail.com>
2020-12-23 06:14:20 +0900

    로그 수정

--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -34,6 +34,13 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
             }
             .disposed(by: rx.disposeBag)
         
+        // Action은 이전 작업이 끝나지 않았을 경우, 다음 작업이 시작되지 않는 것을 보장하고, 오류(Error), 현재 실행상테 등에 관한 Observable를 제공한다.
+        // Action은 다음과 같은 것을 할 수 있다.
+            /// public let inputs: AnyObserver<Input>:  inputs 프로퍼티를 이용하여 수동으로 새로운 작업을 트리거 가능
+            /// public let enabled: Observable<Bool>:  enabled 프로퍼티를 이용하여, 현재 실행중인지 확인가능
+            /// 작업(Work)을 수행하고 Observable의 결과를 반환하는 Factory Closure 호출 됨
+            /// Work Observable에 의해 발생되는 에러 처리 (Gracefully handles error)
+        // typealias CocoaAction = Action<Void, Void>
         addButton.rx.action = viewModel.makeCreateAction()
     }
 
--- RxMemo/ViewModel/MemoListViewModel.swift
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -28,8 +28,16 @@ class MemoListViewModel: CommonViewModel {
         }
     }
     
+    // 참고: public typealias CocoaAction = Action<Void, Void>
+        /// public final class Action<Input, Element>
+        /// public typealias WorkFactory = (Input) -> Observable<Element>
     func makeCreateAction () -> CocoaAction {
+        // 참고: Action 생성자 정의, Action<Void, Void>(enabledIf: , workFactory: )
+        // Trailing Closure로 수행할 작업 (workFactory) 정의
         return CocoaAction { _ in
+            /// 1. 빈 메모생성 :  createMemo에서 새로 생성된 메모 방출
+            /// 2. MemoComposeViewModel 생성 및 화면전환
+            ///     * VM를 생성하면서, 두 가지 액션 (saveAction, cancelAction) 주입
             return self.storage.createMemo(content: "")
                 .flatMap { memo -> Observable<Void> in
                     let memoComposeViewModel = MemoComposeViewModel(title: "새 메모",
@@ -37,8 +45,8 @@ class MemoListViewModel: CommonViewModel {
                                                                     storage: self.storage,
                                                                     saveAction: self.performUpdate(memo: memo),
                                                                     cancelAction: self.performCancel(memo: memo))
-                    
                     let composeScene = Scene.compose(memoComposeViewModel)
+                    // transition메소드는 Completable이 반환하므로, 반환타입에 맞게 observable 반환
                     return self.sceneCoordinator.transition(to: composeScene, style: .modal, animated: true)
                         .asObservable()
                         .map { _ in }
